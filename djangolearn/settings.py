63786ff6a990a6557848406561657bd5c1f173c4
Asma Khan <61154215+Asma-0101@users.noreply.github.com>
2024-11-14 21:27:01 +0530

    Update db in settings.py

--- djangolearn/settings.py
+++ djangolearn/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'djangolearn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'journaldata',
+        'USER': 'root',
+        'PASSWORD': 'admin'
     }
 }
 
